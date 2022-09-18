17efe0246b2d342502694ccf50e224ff7c2f480f
milimolo <bondemand@hotmail.dk>
2022-09-18 11:36:04 +0200

    Boom

---
+++ Simple blockchain/TransactionPool.cs
@@ -0,0 +1,35 @@
+﻿using Simple_blockchain.models;
+
+namespace Simple_blockchain
+{
+    public class TransactionPool
+    {
+        private List<Transaction> rawTransactionList;
+
+        private object lockObj;
+
+        public TransactionPool()
+        {
+            lockObj = new object();
+            rawTransactionList = new List<Transaction>();
+        }
+
+        public void AddRaw(Transaction transaction)
+        {
+            lock (lockObj)
+            {
+                rawTransactionList.Add(transaction);
+            }
+        }
+
+        public List<Transaction> TakeAll()
+        {
+            lock (lockObj)
+            {
+                var all = rawTransactionList.ToList();
+                rawTransactionList.Clear();
+                return all;
+            }
+        }
+    }
+}
