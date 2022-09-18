088aaf88ef503156c2041093a51156ead00850b5
milimolo <bondemand@hotmail.dk>
2022-09-18 18:14:20 +0200

    Commit

--- Simple blockchain/BlockMiner.cs
+++ Simple blockchain/BlockMiner.cs
@@ -6,8 +6,9 @@ namespace Simple_blockchain
 {
     public class BlockMiner
     {
-        public List<Block> Blockchain { get; set; }
-
+        public List<Block>? Blockchain { get; set; }
+        private TransactionPool? transactionPool;
+        private CancellationTokenSource? cancellationToken;
 
         public static string CalculateHash(string rawData)
         {
@@ -25,6 +26,90 @@ namespace Simple_blockchain
                 }
                 return builder.ToString();
             }
+        }
+
+        private string FindMerkleRootHash(IList<Transaction> transactionList)
+        {
+            var transactionStrList = transactionList.Select(tran => 
+                CalculateHash(CalculateHash(tran.From + tran.To + tran.Amount))).ToList();
+            return BuildMerkleRootHash(transactionStrList);
+        }
+
+        private string BuildMerkleRootHash(IList<string> merkleLeaves)
+        {
+            if (merkleLeaves == null || !merkleLeaves.Any())
+                return string.Empty;
+
+            if (merkleLeaves.Count() == 1)
+                return merkleLeaves.First();
+
+            if (merkleLeaves.Count() % 2 > 0)
+                merkleLeaves.Add(merkleLeaves.Last());
+
+            var merkleBranches = new List<string>();
+
+            for (int i = 0; i < merkleLeaves.Count(); i += 2)
+            {
+                var leafPair = string.Concat(merkleLeaves[i], merkleLeaves[i + 1]);
+                merkleBranches.Add(CalculateHash(CalculateHash(leafPair)));
+            }
+            return BuildMerkleRootHash(merkleBranches);
+        }
+
+        private void GenerateBlock()
+        {
+            var lastBlock = Blockchain.LastOrDefault();
+            var block = new Block()
+            {
+                TimeStamp = DateTime.Now,
+                Nounce = 0,
+                TransactionList = transactionPool.TakeAll(),
+                Index = (lastBlock?.Index + 1 ?? 0),
+                PrevHash = lastBlock?.Hash ?? string.Empty
+            };
+            MineBlock(block);
+            Blockchain.Add(block);
+        }
+
+        private void MineBlock(Block block)
+        {
+            var merkleRootHash = FindMerkleRootHash(block.TransactionList);
+            long nounce = -1;
+            var hash = string.Empty;
+            do
+            {
+                nounce++;
+                var rowData = block.Index + block.PrevHash + block.TimeStamp.ToString() + nounce + merkleRootHash;
+                hash = CalculateHash(CalculateHash(rowData));
+            }
+            while (!hash.StartsWith("0000"));
+            block.Hash = hash;
+            block.Nounce = nounce;
+        }
+
+        private void DoGenerateBlock()
+        {
+            while (true)
+            {
+                var startTime = DateTime.Now.Millisecond;
+                GenerateBlock();
+                var endTime = DateTime.Now.Millisecond;
+                var remainTime = 1000 - (endTime - startTime);
+                Thread.Sleep(remainTime < 0 ? 0 : remainTime);
+            }
+        }
+
+        public void Start()
+        {
+            cancellationToken = new CancellationTokenSource();
+            Task.Run(() => DoGenerateBlock(), cancellationToken.Token);
+            Console.WriteLine("Mining has started.");
+        }
+
+        public void Stop()
+        {
+            cancellationToken.Cancel();
+            Console.WriteLine("Mining has stopped.");
         }
     }
 }
